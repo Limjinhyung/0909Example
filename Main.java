1c63629fbf05cfb05ad40c48ba4039647f7177ca
Limjinhyung <89821355+Limjinhyung@users.noreply.github.com>
2021-09-09 12:18:19 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		double[] avg = {85.6, 79.5, 83.1, 80.0, 78.2, 75.0, 80.0, 90.0, 70.0, 60.0};
+		double[] avg = {85.6, 79.5, 83.1, 80.0, 78.2, 75.0};
 		
 		int a = sc.nextInt();
 		int b = sc.nextInt();
